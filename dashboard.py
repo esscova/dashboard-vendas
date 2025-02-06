b555770665425fbca2de5a7dbb8c9bb324df8d4d
wellington moreira <wsantos08@hotmail.com>
2025-02-06 20:10:49 -0300

    add graficos de vendas por categoria e por estado

--- dashboard.py
+++ dashboard.py
@@ -26,11 +26,11 @@ quantidade_de_vendas = formata_numero(dados.shape[0])
 receita_dos_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_dos_estados = dados.drop_duplicates(subset=['Local da compra'])[['Local da compra', 'lat', 'lon']].merge(receita_dos_estados, how='left', left_on='Local da compra', right_index=True).sort_values(by='Preço', ascending=False)
 
-receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))[['Preço']].sum().reset_index()
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))[['Preço']].sum().reset_index()
 receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
 receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name(locale='pt_BR')
 
-# receita_categorias = 
+receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values(by='Preço', ascending=False)
 
 # graficos
 fig_mapa_receita = px.scatter_geo(
@@ -58,6 +58,30 @@ fig_receita_mensal = px.line(
 fig_receita_mensal.update_layout(
     yaxis_title='Receita',
 )
+
+fig_receita_estados = px.bar(
+    receita_dos_estados.head(),
+    x='Local da compra',
+    y='Preço',
+    title='Receita por estado',
+    # color_discrete_sequence=['#fcolormaps'],
+    text_auto=True,
+    template='plotly_white',
+)
+fig_receita_estados.update_layout(
+    yaxis_title='Receita'
+)
+fig_receita_categorias = px.bar(
+    receita_categorias,
+    text_auto=True,
+    title='Receita por categoria',
+    # color_discrete_sequence=['#fcolormaps'],
+    template='plotly_white',
+)
+fig_receita_categorias.update_layout(
+    yaxis_title='Receita'
+)
+
 # streamlit
 st.set_page_config(layout="wide")
 
@@ -67,9 +91,11 @@ col1, col2 = st.columns(2)
 with col1:
     st.metric('Receita', receita)
     st.plotly_chart(fig_mapa_receita, use_container_width=True)
+    st.plotly_chart(fig_receita_estados, use_container_width=True)
 
 with col2:
     st.metric('Quantidade de vendas', quantidade_de_vendas)
     st.plotly_chart(fig_receita_mensal, use_container_width=True)
+    st.plotly_chart(fig_receita_categorias, use_container_width=True)
 
 st.dataframe(dados)
