70b81925982457333a4bc0fd595a44e7fbd55ffd
wellington moreira <wsantos08@hotmail.com>
2025-02-07 19:07:05 -0300

    refactor: modularizacao feat: filtros e botao para persistir dados filtrados em csv

--- src/pages/dados_brutos.py
+++ src/pages/dados_brutos.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-
 # ...
-
 @st.cache_data
 def carrega_dados(caminho):
     return pd.read_parquet(caminho)
@@ -13,38 +11,70 @@ dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/
 
 st.title('Dados brutos')
 
+# atributos selecionados da tabela
 with st.expander('Atributos'):
-    atributos = st.multiselect("Selecione os atributos", dados.columns.tolist(), dados.columns.tolist())
+    atributos = st.multiselect("Selecione as colunas", dados.columns.tolist(), dados.columns.tolist())
 
+# função para criar filtros
+def cria_filtro(df, coluna, tipo='multiselect', titulo=None, padrao=None):
+    with st.sidebar.expander(coluna):
+        if tipo == 'multiselect':
+            return st.multiselect(titulo, df[coluna].unique().tolist(), default=padrao or df[coluna].unique().tolist())
+        elif tipo == 'slider':
+            return st.slider(titulo, int(df[coluna].min()), int(df[coluna].max()), padrao or (int(df[coluna].min()), int(df[coluna].max())))
+        elif tipo == 'date_input':
+            return st.date_input(titulo, padrao or (df[coluna].min(), df[coluna].max()))
+
+# Criando os filtros
 st.sidebar.title('Filtros')
-with st.sidebar.expander('Produto'):
-    produtos = st.multiselect("Selecione os produtos", dados['Produto'].unique().tolist(), dados['Produto'].unique().tolist())
+produtos = cria_filtro(dados, 'Produto', titulo='Selecione os produtos')
+categoria = cria_filtro(dados, 'Categoria do Produto', titulo='Selecione a categoria')
+preco = cria_filtro(dados, 'Preço', tipo='slider', titulo='Selecione o preço', padrao=(0, 5000))
+frete = cria_filtro(dados, 'Frete', tipo='slider', titulo='Selecione o frete')
+data_compra = cria_filtro(dados, 'Data da Compra', tipo='date_input', titulo='Selecione a data')
+vendedor = cria_filtro(dados, 'Vendedor', titulo='Selecione o vendedor')
+local_compra = cria_filtro(dados, 'Local da compra', titulo='Selecione o local')
+avaliacao_compra = cria_filtro(dados, 'Avaliação da compra', tipo='slider', titulo='Selecione a avaliação', padrao=(1, 5))
+tipo_pagamento = cria_filtro(dados, 'Tipo de pagamento', titulo='Selecione o tipo de pagamento')
+quantidade_parcelas = cria_filtro(dados, 'Quantidade de parcelas', tipo='slider', titulo='Selecione a quantidade de parcelas', padrao=(1, 12))
 
-with st.sidebar.expander('Categoria do Produto'):
-    categoria = st.multiselect('Selecione a categoria', dados['Categoria do Produto'].unique().tolist(), dados['Categoria do Produto'].unique().tolist())
+# dict para aplicar filtros em laço
+filtros = {
+    'Produto': produtos,
+    'Categoria do Produto': categoria,
+    'Preço': preco,
+    'Frete': frete,
+    'Data da Compra': data_compra,
+    'Vendedor': vendedor,
+    'Local da compra': local_compra,
+    'Avaliação da compra': avaliacao_compra,
+    'Tipo de pagamento': tipo_pagamento,
+    'Quantidade de parcelas': quantidade_parcelas
+}
+# dict -> laco -> df
+def aplica_filtros(df, filtros):
+    for coluna, valor in filtros.items():
+        if valor:
+            if isinstance(valor, tuple):  # slider ou date_input
+                if coluna == 'Data da Compra':
+                    df = df[(df[coluna].dt.date >= valor[0]) & (df[coluna].dt.date <= valor[1])]
+                else:
+                    df = df[(df[coluna] >= valor[0]) & (df[coluna] <= valor[1])]
+            else:  # multiselect
+                df = df[df[coluna].isin(valor)]
+    return df
 
-with st.sidebar.expander('Preço'):   
-    preco = st.slider('Selecione o preço', 0, 5000, (0, 5000))
+dados_filtrados = aplica_filtros(dados, filtros)
 
-with st.sidebar.expander('Frete'):
-    frete = st.slider('Selecione o frete', 0, int(dados['Frete'].max()), (0, int(dados['Frete'].max())))
+# dados filtrados
+st.dataframe(dados_filtrados[atributos])
+st.markdown(f'Total de registros: :blue[{dados_filtrados.shape[0]}]')
+st.markdown('---')
 
-with st.sidebar.expander('Data da compra'):
-    data_compra = st.date_input('Selecione a data', ((dados['Data da Compra'].min(), dados['Data da Compra'].max())))
-
-with st.sidebar.expander('Vendedor'):
-    vendedor = st.multiselect('Selecione o vendedor', dados['Vendedor'].unique().tolist(), dados['Vendedor'].unique().tolist())
-
-with st.sidebar.expander('Local da compra'):
-    local_compra = st.multiselect('Selecione o local', dados['Local da compra'].unique().tolist(), dados['Local da compra'].unique().tolist())
-
-with st.sidebar.expander('Avaliação da compra'):
-    avaliacao_compra = st.slider('Selecione a avaliação', 1, 5, (1, 5))
-
-with st.sidebar.expander('Tipo de pagamento'):
-    tipo_pagamento = st.multiselect('Selecione o tipo de pagamento', dados['Tipo de pagamento'].unique().tolist(), dados['Tipo de pagamento'].unique().tolist())
-
-with st.sidebar.expander('Quantidade de parcelas'):
-    quantidade_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 12, (1, 12))
-
-st.dataframe(dados)
+# dados filtrados em csv
+st.download_button(
+    label="Download CSV",
+    data=dados_filtrados.to_csv(index=False).encode('utf-8'),
+    file_name='dados.csv',
+    mime='text/csv'
+)
