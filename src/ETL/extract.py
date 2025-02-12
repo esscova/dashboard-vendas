df64fea622bacca9a4433f441fa2cbcc7d1627ae
wellington moreira <wsantos08@hotmail.com>
2025-02-11 23:28:51 -0300

    refactor: extracao de dados

--- src/ETL/extract.py
+++ src/ETL/extract.py
@@ -1,16 +1,32 @@
+import logging
 import requests
 
+logging.basicConfig(level=logging.ERROR)
+
 def extrai_dados(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as err:
-        print(f'Erro ao acessar o endpoint: {url} - {err}')
-        return None
+	"""
+	Extrai dados de uma url e retorna conteudo em JSON
+
+	: param url:  URL do endpoint
+	: return: JSON | None
+	"""
+	try:
+		response = requests.get(url, timeout=10)
+		response.raise_for_status()
+		dados = response.json()
+
+		if not isinstance(dados, list):
+			logging.error('Formato de dados inesperado')
+			return None
+
+		return response.json()
+	
+	except requests.exceptions.RequestException as err:
+		print(f'Erro ao acessar o endpoint: {url} - {err}')
+		return None
 
 
 if __name__ == '__main__':
     url = 'https://labdados.com/produtos'
     dados = extrai_dados(url)
-    print(dados)
+    print(dados) if dados else print('Nenhum dado retornado')
